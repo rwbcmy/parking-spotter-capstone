a36b7ff8929565cbf0045299a2178076341849ac
eabrcr <eabrcr@umsystem.edu>
2026-03-13 18:09:41 -0500

    Added vehicle counting and filtered false positives in inference

--- app/inference.py
+++ app/inference.py
@@ -8,6 +8,8 @@ VIDEO_PATH = os.getenv("VIDEO_PATH", os.path.join(BASE_DIR, "videos", "Car-Parki
 MODEL_PATH = os.getenv("MODEL_PATH", "yolo12n.pt")
 
 
+
+
 def main():
     model = YOLO(MODEL_PATH)
     cap = cv2.VideoCapture(VIDEO_PATH)
@@ -18,18 +20,52 @@ def main():
 
     print(f"Input video: {VIDEO_PATH}")
 
+    frame_skip = 2
+    frame_count = 0
+
     while True:
         ret, frame = cap.read()
         if not ret:
             break
+        frame_count += 1
+        if frame_count % frame_skip != 0:
+            continue
 
         h, w = frame.shape[:2]
         new_w = 640
         new_h = int(h * (new_w / w))
         frame = cv2.resize(frame, (new_w, new_h))
 
-        results = model(frame, verbose=False, conf=0.2, classes=[2, 3, 5, 7])
+        results = model(frame, verbose=False, conf=0.2)
+
+        #Count the detected number of vehicles
+        boxes = results[0].boxes
+        vehicle_count = 0
+
+        for box in boxes:
+            cls = int(box.cls[0])
+            label = model.names[cls]
+
+            # Ignore false "clock" detections
+            if label not in ["clock", "cell phone"]:   
+                vehicle_count += 1
+
+        print("Vehicles detected:", vehicle_count)
+        
         annotated_frame = results[0].plot()
+
+        total_spaces = 7
+        available_spaces = total_spaces - vehicle_count
+
+        cv2.putText(
+            annotated_frame,
+            f"Occupied: {vehicle_count}  Available: {available_spaces}",
+            (10, 40),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 255, 0),
+            2
+)
 
         cv2.imshow("YOLO Inference", annotated_frame)
 
